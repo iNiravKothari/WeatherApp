48208446e0b0491dbf5c879202725eb1d9ba5358
Nirav Kothari <kothari.nirav@gmail.com>
2015-01-29 01:30:29 +0530

    All changes done till 2nd chapter. Added cloud call to fetch weather data

--- app/src/main/java/com/example/nirav/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/nirav/weatherapp/MainActivity.java
@@ -1,19 +1,9 @@
 package com.example.nirav.weatherapp;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -50,74 +40,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String[] forecastArray = {
-                    getString(R.string.today_weather_str),
-                    "Tomorrow - Foggy - 22",
-                    "Wed - Cloudy - 23",
-                    "Thurs - Rainy - 24",
-                    "Fri - Heavy Rain - 26",
-                    "Sat - Bright - 30",
-                    "Sun - Rain - 24"
-            };
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-
-            ArrayAdapter<String> adapterArray = new ArrayAdapter<String>(getActivity(),
-                                                                        R.layout.fragment_list_item_forecast,
-                                                                        R.id.list_item_forecast_textview,
-                                                                        weekForecast);
-
-            ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(adapterArray);
-
-            try {
-/*              URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=7");
-                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-*/
-                /*InputStream inputStream = urlConnection.getInputStream();
-                StringBuilder buffer = new StringBuilder();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line).append("\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                String forecastJsonStr = buffer.toString();
-                */
-            }
-            catch(Exception e)
-            {
-
-            }
-
-            return rootView;
-        }
     }
 }
